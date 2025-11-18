add46399355169e86ad65bc7901497fdb29f57e7
aldogg10 <179862@upslp.edu.mx>
2025-11-17 22:28:11 -0600

    Create 01_create_tables.sql

---
+++ tests/01_create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    atributos JSONB
+);
+
+
+CREATE TABLE usuarios (
+  id SERIAL PRIMARY KEY,
+  data JSONB
+);
