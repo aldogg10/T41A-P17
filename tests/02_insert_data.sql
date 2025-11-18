89c400fb67e23a87ef2c0fb8e1051ac03b1e4ea1
aldogg10 <179862@upslp.edu.mx>
2025-11-17 22:29:42 -0600

    Create 02_insert_data.sql

---
+++ tests/02_insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO productos (nombre, atributos)
+VALUES
+('Laptop', '{"marca": "Dell", "ram": "16GB"}'),
+('Teléfono', '{"marca": "Samsung", "color": "Negro"}'),
+('PS5', '{"marca": "Sony", "Espacio": "1TB"}');
+
