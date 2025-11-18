2c1e1d18d958e50c12a798dd5163194a3f1cc0a0
aldogg10 <179862@upslp.edu.mx>
2025-11-17 22:12:06 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -2,3 +2,8 @@ INSERT INTO usuarios (data)
 VALUES 
   ('{"nombre": "Ana", "activo": true, "edad": 30}'),
   ('{"nombre": "Juan", "activo": false, "edad": 25}');
+
+INSERT INTO productos (nombre, atributos)
+VALUES 
+  ('Laptop', 'marca => Dell, color => negro, ram => 16GB'),
+  ('Teléfono', 'marca => Samsung, color => azul, ram => 8GB');
