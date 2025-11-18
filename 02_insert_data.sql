06c876d6d5c0bdec5addbbf05d5fbff715cb1022
aldogg10 <179862@upslp.edu.mx>
2025-11-17 22:15:50 -0600

    Update and rename 02_insert_data.sql to 03_insert_data.sql

--- 02_insert_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO usuarios (data)
-VALUES 
-  ('{"nombre": "Ana", "activo": true, "edad": 30}'),
-  ('{"nombre": "Juan", "activo": false, "edad": 25}');
-
-INSERT INTO productos (nombre, atributos)
-VALUES 
-  ('Laptop', 'marca => Dell, color => negro, ram => 16GB'),
-  ('Teléfono', 'marca => Samsung, color => azul, ram => 8GB');
---
+++ 03_insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO productos (nombre, atributos)
+VALUES 
+  ('Laptop', 'marca => Dell, color => negro, ram => 16GB'),
+  ('Teléfono', 'marca => Samsung, color => azul, ram => 8GB'),
+  ('PS5', 'marca => Sony, color => negro, ram => 32GB'),
+  ('Xbox Rog Ally', 'marca => Azus, color => blanca, ram => 16GB'),
+  ('Impresora', 'marca => HP, color => roja, ram => 2GB');
